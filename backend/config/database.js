d55d5a420d1c959b43082db95e7e021febe712f6
Audric Matthew <c14220332@john.petra.ac.id>
2026-02-01 17:45:52 +0800

    add port at database.js (const mysql)

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 // Create connection pool
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'cahaya_phone_crm',
